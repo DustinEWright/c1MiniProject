cf435a08aff561673f574d1a4c47abb983fa6b2d
Dustin Wright <nzeroxpd@gmail.com>
2017-03-24 03:04:06 +0000

    Duke C:01-W:04 Mini-Project: Prototype

---
+++ script.js
@@ -0,0 +1,22 @@
+// Load the original image:
+function loadImage() {
+  alert("Image Loaded.")
+}
+
+// Filter Effects:
+function grayScale() {
+  alert("Grayscale Filter Applied.")
+}
+
+function redFilter() {
+  alert("Red Filter Applied.")
+}
+
+function rainbowFilter() {
+  alert("Rainbow Filter Applied.")
+}
+
+// Reset the image back to the original version
+function resetImage() {
+  alert("Image Reset To Original.");
+}
